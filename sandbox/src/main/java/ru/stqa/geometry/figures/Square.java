8f7a0c19ff4ff4242f38352a26073a8afe13bf90
Ivan <imeshcheriyakov@gmail.com>
2023-10-03 18:46:40 +0300

    Структурирован код

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,7 +2,8 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side){
-        System.out.println("Площадь квадрата со стороной " + side + " = " + sqareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, sqareArea(side));
+        System.out.println(text);
     }
 
     private static double sqareArea(double a) {
